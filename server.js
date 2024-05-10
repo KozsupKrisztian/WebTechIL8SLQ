924336e48bbdec34f45f311688610c53621e245e
KozsupKrisztian <kozsup.krisztian@gmail.com>
2024-05-10 08:09:48 +0200

    asddsa

--- server.js
+++ server.js
@@ -4,13 +4,21 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Statikus fájlok szolgáltatása
 app.use(express.static('public'));
 
-
-// Főoldal megjelenítése
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// 404 - Oldal nem található
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'errors', '404.html'));
+});
+
+// Globális hibakezelés
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).sendFile(path.join(__dirname, 'errors', '500.html'));
 });
 
 app.listen(port, () => {
